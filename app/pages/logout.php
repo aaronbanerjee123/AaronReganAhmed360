f45d7a2ce2562218ccd1e2674fe4c921566f5005
aaronbanerjee123 <aaronbaner@outlook.com>
2024-03-26 13:43:51 -0700

    latest commit

--- app/pages/logout.php
+++ app/pages/logout.php
@@ -2,10 +2,10 @@
 include __DIR__ . '/../core/init.php';
  if(!empty($_SESSION['USER'])){
     unset($_SESSION['USER']);
-   //  session_destroy();
+    session_destroy();
  }
  header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
  header("Pragma: no-cache"); // HTTP 1.0.
  header("Expires: 0"); 
- redirect_login();
-// header('../pages/login.php');
+//  redirect_login();
+header('../pages/login.php');
