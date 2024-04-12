071c1d6ec7080e80845392ab859ba1bd867e96a0
aaronbanerjee123 <aaronbaner@outlook.com>
2024-04-11 20:49:00 -0700

    move edit button

--- app/pages/includes/post-card.php
+++ app/pages/includes/post-card.php
@@ -2,8 +2,13 @@
     <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style="width: 100%; height: 250px;">
         <!-- Image Column -->
         <div class="col-lg-5 col-12 d-lg-block">
-            <img src="<?=ROOT?>pages/<?=$row['image']?>" class="bd-placeholder-img w-100" style="height: 100%; object-fit: cover; width:100px; height:100px;" />
+            <img src="<?=ROOT?>pages/<?=$row['image']?>" class="bd-placeholder-img w-100" style="height: 100%; object-fit: cover; width:100px; height:200px;" />
         </div>
+
+        <!-- Edit Button -->
+        <?php if($row['user_id'] == user('id')) { ?>
+            <a href="<?=ROOT?>pages/edit.php?id=<?=$row['id']?>" class="btn btn-sm btn-primary position-absolute bottom-0 start-0 mt-5" style="width: 20%;">Edit</a>
+        <?php } ?>
 
         <!-- Content Column -->
         <div class="col p-4 d-flex flex-column position-static" style="width: calc(100% - 340px);">
@@ -19,11 +24,11 @@
             <div class="mb-1 text-muted"><?=date("Y-m-d",strtotime($row['date']))?></div>
 
             <!-- Truncated Content -->
-            <p class="card-text mb-auto" id="truncatedContent"><?=substr($row['content'],0,50)?></p>
+            <p class="card-text mb-auto" id="truncatedContent"><?=substr($row['content'],0,30)?></p>
 
             <!-- Full content (initially hidden) -->
             <div class="full-content" style="display: none;">
-                <p><?= $row['content'] ?></p>
+                <p><?=substr($row['content'],0,80)?></p>
             </div>
 
             <!-- Read more button -->
@@ -31,10 +36,5 @@
                 <a href="#" class="read-more" style="width: 75px; display: inline-block; background-color: #007bff; color: #fff; border: none; padding: 8px 20px; text-align: center; text-decoration: none; border-radius: 4px; cursor: pointer; transition: background-color 0.3s ease;">Read more</a>
             <?php } ?>
         </div>
-
-        <!-- Edit Button -->
-        <?php if($row['user_id'] == user('id')) { ?>
-            <a href="<?=ROOT?>pages/edit.php?id=<?=$row['id']?>" class="btn btn-sm btn-primary position-absolute bottom-0 end-0 mt-5" style="width: 20%;">Edit</a>
-        <?php } ?>
     </div>
 </div>
