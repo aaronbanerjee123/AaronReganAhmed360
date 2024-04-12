68f9f7271d486a219350bf677dd2da5bf3d6326d
aaronbanerjee123 <aaronbaner@outlook.com>
2024-04-11 22:00:16 -0700

    added alert on the the comment history header (regan)

--- app/pages/commentHistory.php
+++ app/pages/commentHistory.php
@@ -95,13 +95,29 @@ $rows = query($query, ['user_id' => $_SESSION['USER']['id']]);
             <li><a class="dropdown-item" href="<?=ROOT?>pages/commentHistory.php">Comment History</a></li>
 
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="<?=ROOT?>pages/logout.php">Sign out</a></li>
+            <li><a class="dropdown-item" href="#" onclick="confirmSignOut()">Sign out</a></li>
           </ul>
         </div>
         <?php } ?>
       </div>
     </div>
   </header>
+
+  <div id="confirmationModal" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body">
+        <p>Are you sure you want to sign out?</p>
+        <div class="text-center">
+          
+          <button id="yesButton" class="btn btn-danger" onclick="signOut()">Yes</button>
+          <button id="noButton" class="btn btn-secondary ms-2" data-bs-dismiss="modal">No</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 
@@ -158,6 +174,23 @@ $rows = query($query, ['user_id' => $_SESSION['USER']['id']]);
 
 </main>
     <script src="<?=ROOT?>public/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<script>
+
+  function confirmSignOut() {
+
+    $('#confirmationModal').modal('show');
+  }
+
+
+  function signOut() {
+
+    window.location.href = '<?=ROOT?>pages/logout.php';
+  }
+</script>
+
+
   </body>
 </html>
 
